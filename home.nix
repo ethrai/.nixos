2a2f598222223ebd04a9cfa2f6b9192e55f91319
Sergei Ryzhko <ethrai@proton.me>
2024-08-02 12:47:52 +0300

    update

--- home.nix
+++ home.nix
@@ -24,6 +24,14 @@
     themes = { };
   };
 
+  programs.chromium = {
+    enable = true;
+    extensions = [
+      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; }
+      { id = "dbepggeogbaibhgnhhndojpepiihcmeb"; }
+    ];
+  };
+
   programs.btop = { enable = true; };
 
   home.packages = with pkgs; [
@@ -49,6 +57,7 @@
     btop
     gh
     neovim
+    yt-dlp
   ];
 
   home.stateVersion = "24.05"; # Please read the comment before changing.
@@ -75,5 +84,39 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  programs.hyprlock.enable = true;
+  programs.hyprlock = {
+    enable = true;
+    settings = {
+      general = {
+      disable_loading_bar = true;
+      grace = 300;
+      hide_cursor = true;
+      no_fade_in = false;
+    };
+
+    background = [
+      {
+        path = "screenshot";
+        blur_passes = 3;
+        blur_size = 8;
+      }
+    ];
+
+    input-field = [
+      {
+        size = "200, 50";
+        position = "0, -80";
+        monitor = "";
+        dots_center = true;
+        fade_on_empty = false;
+        font_color = "rgb(202, 211, 245)";
+        inner_color = "rgb(91, 96, 120)";
+        outer_color = "rgb(24, 25, 38)";
+        outline_thickness = 5;
+        placeholder_text = '\'<span foreground="##cad3f5">Password...</span>'\';
+        shadow_passes = 2;
+      }
+    ];
+    };
+  };
 }
